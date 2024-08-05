76c958f9cffba73c883a43dd4b01ababf360294f
sidhanthembrom <hembrom.sidhant@gmail.com>
2024-08-05 15:22:12 +0530

    added video-wrapper and scrim-opacity in HTML, related CSS, JS for opacity of section videos

--- index.js
+++ index.js
@@ -16,16 +16,22 @@ const bannerVideo = document.querySelector(".bannerVideo");
 
 // Used for nav clicking
 const techNav = document.querySelector(".techNav");
-const bookNav = document.querySelector(".value")
+const bookNav = document.querySelector(".value");
 
+// Used for Dynamic Opacity
+const opacityContainers = document.querySelectorAll(".scrim-opacity");
+
+// main
+const sectionMainHeadings = document.querySelectorAll(".section-main-heading");
+// main
 
 // *** CODING *** //
 
 // Changing navbar color & sectionVideos width dynamically as the page is scrolled
 window.addEventListener("scroll", () => {
   // console.log(technology.getBoundingClientRect().top)
-  let diffTop = navbar.offsetHeight - technology.getBoundingClientRect().top;
-  let diffBottom = technology.getBoundingClientRect().bottom;
+  let diffTop = navbar.offsetHeight - technology.getBoundingClientRect().top; //traks where top is wrt navabr (generally viewport's top)
+  let diffBottom = technology.getBoundingClientRect().bottom; //tracks where bottom is wrt viewport's top
 
   //   console.log(diffTop);
   //   console.log(diffBottom);
@@ -46,7 +52,7 @@ window.addEventListener("scroll", () => {
       //   console.log("white");
     }
   }
-  // When black theme is still there for lg devices and the above if condition fails
+  // When black theme is still there for lg devices and the above 'if' condition fails
   else if (diffTop > diffBottom && diffBottom > 0) {
     navbar.classList.add("bg-black");
     navbar.setAttribute("data-bs-theme", "dark");
@@ -105,6 +111,70 @@ window.addEventListener("scroll", () => {
       // console.log("pause");
     }
   });
+
+  // opacity for section videos
+  opacityContainers.forEach((opacityContainer) => {
+    // for first opactity container only
+    if (opacityContainer == opacityContainers[0]) {
+      const sectionHeadingBottom =
+        sectionMainHeadings[0].getBoundingClientRect().bottom - 66; //adjustment for navbar & spacings
+      const opacityPercentage = (sectionHeadingBottom / 450) * 0.6;
+
+      if (sectionHeadingBottom < 450 && sectionHeadingBottom >= 0) {
+        opacityContainers[0].style.backgroundColor = `rgba(0, 0, 0, ${opacityPercentage})`;
+      }
+    }
+    // for second opacity container only
+    else if (opacityContainer == opacityContainers[1]) {
+      const sectionHeadingBottom =
+        sectionMainHeadings[2].getBoundingClientRect().bottom - 66; //adjustment for navbar & spacings
+      const opacityPercentage = (sectionHeadingBottom / 450) * 0.6;
+
+      if (sectionHeadingBottom < 450 && sectionHeadingBottom >= 0) {
+        opacityContainers[1].style.backgroundColor = `rgba(0, 0, 0, ${opacityPercentage})`;
+      }
+    }
+    // for third opacity container only
+    else if (opacityContainer == opacityContainers[2]) {
+      const sectionHeadingBottom =
+        sectionMainHeadings[4].getBoundingClientRect().bottom - 66; //adjustment for navbar & spacings
+      const opacityPercentage = (sectionHeadingBottom / 450) * 0.6;
+
+      if (sectionHeadingBottom < 450 && sectionHeadingBottom >= 0) {
+        opacityContainer.style.backgroundColor = `rgba(0, 0, 0, ${opacityPercentage})`;
+      }
+    }
+    // for fourth opacity container only
+    else if (opacityContainer == opacityContainers[3]) {
+      const sectionHeadingBottom =
+        sectionMainHeadings[6].getBoundingClientRect().bottom - 66; //adjustment for navbar & spacings
+      const opacityPercentage = (sectionHeadingBottom / 450) * 0.6;
+
+      if (sectionHeadingBottom < 450 && sectionHeadingBottom >= 0) {
+        opacityContainer.style.backgroundColor = `rgba(0, 0, 0, ${opacityPercentage})`;
+      }
+    }
+    // for fifth opacity container only
+    else if (opacityContainer == opacityContainers[4]) {
+      const sectionHeadingBottom =
+        sectionMainHeadings[8].getBoundingClientRect().bottom - 66; //adjustment for navbar & spacings
+      const opacityPercentage = (sectionHeadingBottom / 450) * 0.6;
+
+      if (sectionHeadingBottom < 450 && sectionHeadingBottom >= 0) {
+        opacityContainer.style.backgroundColor = `rgba(0, 0, 0, ${opacityPercentage})`;
+      }
+    }
+    // for sixth opacity container only
+    else if (opacityContainer == opacityContainers[5]) {
+      const sectionHeadingBottom =
+        sectionMainHeadings[10].getBoundingClientRect().bottom - 66; //adjustment for navbar & spacings
+      const opacityPercentage = (sectionHeadingBottom / 450) * 0.6;
+
+      if (sectionHeadingBottom < 450 && sectionHeadingBottom >= 0) {
+        opacityContainer.style.backgroundColor = `rgba(0, 0, 0, ${opacityPercentage})`;
+      }
+    }
+  });
 });
 
 // Changing active class to the clicked nav from the previous nav
@@ -129,7 +199,7 @@ bannerVideo.addEventListener("timeupdate", () => {
   }
 });
 
-// scrolling to a certain part when clicked 
+// scrolling to a certain part when clicked
 const isDesktop = window.matchMedia("(min-width: 1024px)").matches; //keeps watch over the device width
 if (isDesktop) {
   techNav.addEventListener("click", (e) => {
@@ -138,16 +208,21 @@ if (isDesktop) {
     window.scrollTo({
       top: 17200,
       left: 0,
-      behavior: "smooth" // Smooth scrolling
+      behavior: "smooth", // Smooth scrolling
     });
   });
-  
+
   bookNav.addEventListener("click", (e) => {
     e.preventDefault();
     window.scrollTo({
       top: 21700,
       left: 0,
-      behavior: "smooth"
-    })
+      behavior: "smooth",
+    });
   });
 }
+
+// opacity for section videos
+window.addEventListener("scroll", () => {
+  
+});
